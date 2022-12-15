c215c9a77d91a55aa5fbae23b0f22153717c3774
im3dabasia <eshaandabasiya@gmail.com>
2022-12-15 23:27:54 +0530

    Updated the user.models.js

--- server/models/user.models.js
+++ server/models/user.models.js
@@ -1,8 +1,18 @@
 const mongoose = require("mongoose");
+import { isEmail } from 'validator';
 
 const userSchema = new mongoose.Schema({
-  email: String,
-  password: String,
+  userName: String,
+  email: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: 'Email address is required',
+    validate: [ isEmail, 'invalid email']
+  },
+  passWord: String,
+  googleId: String,
+  hash: String,
 });
 
 module.exports = new mongoose.model("User", userSchema);
