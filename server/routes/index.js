bd9dbf2bcd6c9c10134367adfc0639193537f21b
Harsh Bhagat <93080554+BhagatHarsh@users.noreply.github.com>
2022-12-17 05:49:42 +0000

    addedJWT

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+router.use('/auth', require('./signup.route'));
 router.use('/auth', require('./signin.route'));
+
 
 module.exports = router;
--- server/routes/signup.route.js
+++ server/routes/signup.route.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router()
 const authController = require('../controllers/auth.controller');
 
-router.post('/signin', authController.signin);
+router.post('/signup', authController.signup);
 
 module.exports = router;
