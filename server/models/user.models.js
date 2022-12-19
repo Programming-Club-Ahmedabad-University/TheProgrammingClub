9dbf12f1b7070a00652472785dbed1905458bad1
im3dabasia <eshaandabasiya@gmail.com>
2022-12-20 00:43:54 +0530

    Made the suggested changes, added name attribute

--- server/models/user.models.js
+++ server/models/user.models.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  email: String,
-  password: String,
+  name: String,
+  userName: String,
+  email:{
+    type: String,
+    lowercase: true,
+    unique: true,
+    validate: [ isEmail, 'invalid email'] 
+  },
+  passWord: String,
+  googleId: String,
 });
 
 module.exports = new mongoose.model("User", userSchema);
