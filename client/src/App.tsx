8c6a486a4490f04ba75e8b2ac76ffbd38cbf260e
JeelRajodiya <jeelrajodiyajeel@gmail.com>
2022-12-30 19:44:19 +0530

    test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom'; 
+import React from "react"
+import { Routes, Route } from "react-router-dom"
 
-import About from './pages/About';
-import Events from './pages/Events';
-import Community from './pages/Community';
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
+import About from "./pages/About"
+import Events from "./pages/Events"
+import Community from "./pages/Community"
+import Login from "./pages/Login"
+import SignUp from "./pages/SignUp"
 
-import Layout from './components/Layout'; 
+import Layout from "./components/Layout"
 
 const App: React.FC = () => {
-    return (
-        <Routes>
-            <Route element={<Layout />}>
-                <Route path='/' element={<About />} />
-                <Route path='/events' element={<Events />} />
-                <Route path='/community' element={<Community />} />
-                <Route path='/login' element={<Login />} />
-                <Route path='/signup' element={<SignUp />} />
-            </Route>
-        </Routes>
-    );
+	return (
+		<Routes>
+			<Route element={<Layout />}>
+				<Route path="/" element={<About />} />
+				<Route path="/events" element={<Events />} />
+				<Route path="/community" element={<Community />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/signup" element={<SignUp />} />
+			</Route>
+		</Routes>
+	)
 }
 
-export default App; 
+export default App
